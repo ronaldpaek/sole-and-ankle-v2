67d7caa9bf2611b45cfbf075b94551ee44383e25
Ronald Paek <ronaldpaek@gmail.com>
2025-02-16 19:46:48 -0800

    Enhance ShoeCard component with sale and new release flags, and improve layout in ShoeGrid

--- src/components/ShoeCard/ShoeCard.jsx
+++ src/components/ShoeCard/ShoeCard.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 import { COLORS, WEIGHTS } from '../../constants';
-import { formatPrice, pluralize, isNewShoe } from '../../utils';
+import { formatPrice, isNewShoe, pluralize } from '../../utils';
 import Spacer from '../Spacer';
 
 const ShoeCard = ({
@@ -31,6 +31,19 @@ const ShoeCard = ({
       ? 'new-release'
       : 'default'
 
+  let flag;
+  let backgroundColor;
+  if (variant === 'on-sale') {
+    flag = 'Sale';
+    backgroundColor = 'hsla(340, 65%, 47%, 1)';
+  } else if (variant === 'new-release') {
+    flag = 'Just released!';
+    backgroundColor = 'hsla(240, 60%, 63%, 1)';
+  } else if (variant === 'default') {
+    flag = '';
+    backgroundColor = '';
+  }
+
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
@@ -40,11 +53,19 @@ const ShoeCard = ({
         <Spacer size={12} />
         <Row>
           <Name>{name}</Name>
-          <Price>{formatPrice(price)}</Price>
+          <Price variant={variant}>{formatPrice(price)}</Price>
         </Row>
         <Row>
           <ColorInfo>{pluralize('Color', numOfColors)}</ColorInfo>
+          <SalePrice>{salePrice && formatPrice(salePrice)}</SalePrice>
         </Row>
+        <Flag
+          style={{
+            '--background-color': backgroundColor,
+          }}
+        >
+          {flag}
+        </Flag>
       </Wrapper>
     </Link>
   );
@@ -53,18 +74,25 @@ const ShoeCard = ({
 const Link = styled.a`
   text-decoration: none;
   color: inherit;
+  flex: 1 1 340px;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  position: relative;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
 `;
 
-const Image = styled.img``;
+const Image = styled.img`
+  width: 100%;
+`;
 
 const Row = styled.div`
   font-size: 1rem;
+  display: flex;
+  justify-content: space-between;
 `;
 
 const Name = styled.h3`
@@ -72,7 +100,11 @@ const Name = styled.h3`
   color: ${COLORS.gray[900]};
 `;
 
-const Price = styled.span``;
+const Price = styled.span`
+  text-decoration: ${(p) => {
+    return p.variant === 'on-sale' ? 'line-through' : 'none';
+  }};
+`;
 
 const ColorInfo = styled.p`
   color: ${COLORS.gray[700]};
@@ -81,6 +113,22 @@ const ColorInfo = styled.p`
 const SalePrice = styled.span`
   font-weight: ${WEIGHTS.medium};
   color: ${COLORS.primary};
+`;
+
+const Flag = styled.div`
+  position: absolute;
+  top: 0;
+  right: 0;
+  height: 32px;
+  color: hsla(0, 0%, 100%, 1);
+  font-size: 14px;
+  font-weight: 700;
+  background-color: var(--background-color);
+  border-radius: 2px;
+  margin-right: -4px;
+  padding: 9px;
+  display: flex;
+  align-items: center;
 `;
 
 export default ShoeCard;
